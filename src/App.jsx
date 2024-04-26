f4726bfd4edfcace11a8108307f6023fb6562342
ABDULBASIT <abdul.basit@mage4.com>
2024-04-26 18:18:43 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./Layout";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Layout>
+
+      </Layout>
+    </>
+  )
+}
+
+export default App
